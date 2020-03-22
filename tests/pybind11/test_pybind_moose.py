d5d29bb38ceeae7d62bacd23ccad641b9be6543c
Dilawar Singh <dilawars@ncbs.res.in>
2020-03-22 23:12:58 +0530

    Time to support / in path.

--- tests/pybind11/test_pybind_moose.py
+++ tests/pybind11/test_pybind_moose.py
@@ -1,5 +1,7 @@
 import moose._moose as M
 print("Using _moose from %s" % M.__file__)
 print(dir(M))
-a = M.create('/a', 'Neutral', 1)
-print(a)
+a1 = M.create('a1', 'Neutral', 1)
+print(a1)
+a2 = M.create('a2', 'Neutral', 1)
+print(a2)
