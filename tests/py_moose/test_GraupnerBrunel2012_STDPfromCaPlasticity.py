7de32283c4aad0c0a4c9f1bc60af307b9d516712
Dilawar Singh <dilawars@ncbs.res.in>
2020-03-31 19:13:51 +0530

    Minor tweak to a test.

--- tests/py_moose/test_GraupnerBrunel2012_STDPfromCaPlasticity.py
+++ tests/py_moose/test_GraupnerBrunel2012_STDPfromCaPlasticity.py
@@ -40,12 +40,6 @@ def test_GB2012_STDP():
 
     ## two neurons: index 0 will be presynaptic, 1 will be postsynaptic
     network = moose.LIF( 'network', 2 );
-
-    print(network)
-    quit()
-
-
-
     moose.le( '/network' )
     network.vec.Em = Vrest
     network.vec.thresh = Vt_base
